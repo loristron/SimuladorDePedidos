f7a07a654c627add34634b39621dbb2dfc0991ed
loristron <loremmiranda@gmail.com>
2021-03-06 13:28:42 -0300

    Removendo opção cliente da tabela produto

--- src/pedidos/models.py
+++ src/pedidos/models.py
@@ -10,11 +10,10 @@ class Cliente(models.Model):
 #Modelo de validação da classe produtos
 class Produto(models.Model):
 	nome 			= models.CharField(max_length=200, null=True)
-	cliente 		= models.ForeignKey(Cliente, on_delete=models.CASCADE, null=True)
 	quantidade		= models.PositiveIntegerField(null=True, validators=[MinValueValidator(1)])
 	preço_unitario	= models.DecimalField(decimal_places=2, validators=[MinValueValidator(0.01)], null=True, max_digits=100)
 	multiplo 		= models.PositiveIntegerField(null=True, blank=True, validators=[MinValueValidator(1)])
 
 	def __str__(self):
 		return self.nome
-		
+
