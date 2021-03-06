017b923c5e38a80e231fa2e9ac43ae345cbd302e
loristron <loremmiranda@gmail.com>
2021-03-06 20:57:08 -0300

    Sinal pre salvamento com calculo de preço total do pedido e total de itens adicionados

--- src/pedidos/signals.py
+++ src/pedidos/signals.py
@@ -1,10 +1,10 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, m2m_changed
 from django.dispatch import receiver
 
 from random import randint
 import decimal
 
-from .models import Produto, Item, Cliente
+from .models import Item, Pedido
 
 #Recebe sinal e antes de salvar objetos da classe Item, calcula um preço sugerido para o item e 
 #de acordo com esse preço, ou com qualquer preço que seja altarado a partir daí, classifica a rentabilidade
@@ -26,6 +26,19 @@ def pre_save_preço_sugerido(sender, instance, **kwargs):
 	if instance.preço_item <= instance.produto.preço_unitario - decimal.Decimal((float(instance.produto.preço_unitario) * 0.1)):
 		 instance.rentabilidade = 'Ruim'
 	
+@receiver(m2m_changed, sender=Pedido.items.through)
+def pre_save_preço_total_pedido(sender, instance, action, **kwargs):
+	total_price = 0
+	total_items = 0
 
+	if action =='post_add' or action == 'post_remove':
+		
+		for item in instance.items.all():
+			total_items 	+= 1 
+			total_price 	+= item.preço_item 
+		
+		instance.total_price = total_price
+		instance.total_items = total_items
+		instance.save()
 
 	
