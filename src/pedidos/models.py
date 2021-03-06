7a5971786f16e86ce6926f57a9a909a00ddc8a39
loristron <loremmiranda@gmail.com>
2021-03-06 13:16:50 -0300

    Inicialização Tabelas Cliente e Produtos

--- src/pedidos/models.py
+++ src/pedidos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+# Modelo dos Clientes no banco de dados 
+class Cliente(models.Model):
+	nome 	= models.CharField(max_length=200, null=True)
 
-# Create your models here.
+class Produto(models.Model):
+	nome 			= models.CharField(max_length=200, null=True)
+	quantidade		= models.IntegerField
+	preço_unitario	= models.DecimalField(decimal_places=2, validators=[MinValueValidator(0.01)], null=True, max_digits=100)
+	multiplo 		= models.PositiveIntegerField(null=True, blank=True, validators=[MinValueValidator(1)])
