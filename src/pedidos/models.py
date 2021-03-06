e3d9017226f98e6d7e9307e6966cb26181eb041d
loristron <loremmiranda@gmail.com>
2021-03-06 13:36:48 -0300

    Criação da Tabela Produtos

--- src/pedidos/models.py
+++ src/pedidos/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator
+
 # Modelo dos Clientes no banco de dados 
 class Cliente(models.Model):
 	nome 	= models.CharField(max_length=200, null=True)
@@ -15,4 +16,16 @@ class Produto(models.Model):
 
 	def __str__(self):
 		return self.nome
+
+#Modelo de validação da classe pedido
+class Pedido(models.Model):
+	cliente 		= models.ForeignKey(Cliente, on_delete=models.CASCADE, null=True)
+	produto 		= models.ForeignKey(Produto, on_delete=models.CASCADE, null=True)
+	quantidade		= models.PositiveIntegerField(null=True, validators=[MinValueValidator(1)])
+	preço_final		= models.DecimalField(null=True, decimal_places=2, max_digits=100)
+
+	def __str__(self):
+		return f'{self.cliente.nome} - {self.produto.nome}'
+
+
 
