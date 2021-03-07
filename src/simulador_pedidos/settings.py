c1cef81758a273be43023e8d5c21607d0cd9ad34
loristron <loremmiranda@gmail.com>
2021-03-07 18:06:50 -0300

    Instalação Crispy Forms

--- src/simulador_pedidos/settings.py
+++ src/simulador_pedidos/settings.py
@@ -38,8 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #third party apps
+    'crispy_forms',
+
     'pedidos',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
