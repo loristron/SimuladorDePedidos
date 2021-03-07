81c99c6c1ff9bc3a1df258176fcf54d722ffff50
loristron <loremmiranda@gmail.com>
2021-03-07 18:10:09 -0300

    Inclusão de Endpoint para atualização de pedidos

--- src/pedidos/urls.py
+++ src/pedidos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import dashboard_view
+from .views import dashboard_view, update_pedido_view
 
 app_name = 'pedidos'
 
 urlpatterns = [
     path('', dashboard_view, name='dashboard'),
+    path('update-pedido=<int:pk>', update_pedido_view, name='update-pedido'),
 ]
